317dfc60497401ff8051dd43c23a146a76a403a5
Tiffany Mwangi <tiffany.mwagi@student.moringaschool.com>
2023-07-12 13:40:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Convert the word to lowercase
+  const lowercaseWord = word.toLowerCase();
+
+  // Get the length of the word
+  const length = lowercaseWord.length;
+
+  // Iterate over the first half of the word
+  for (let i = 0; i < length / 2; i++) {
+    // Compare the character at index i with the character at the corresponding index from the end
+    if (lowercaseWord[i] !== lowercaseWord[length - 1 - i]) {
+      return false; // Characters don't match, not a palindrome
+    }
+  }
+
+  return true; // All characters match, it's a palindrome
 }
 
-/* 
-  Add your pseudocode here
-*/
-
 /*
-  Add written explanation of your solution here
+  Pseudocode:
+  - Convert the word to lowercase.
+  - Get the length of the word.
+  - Iterate over the first half of the word (until length / 2):
+    - Compare the character at index i with the character at the corresponding index from the end (length - 1 - i).
+    - If the characters don't match, return false (not a palindrome).
+  - If all characters match, return true (palindrome).
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
